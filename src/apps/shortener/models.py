9c43fc559a571c3cbe6dcbcdaa4e42ce51532d1a
Mohsen <mim.khodabakhshi@gmail.com>
2024-12-16 23:31:25 +0330

    ♻️ Refactor on shortened url model

--- src/apps/shortener/models.py
+++ src/apps/shortener/models.py
@@ -4,8 +4,9 @@ from utils.common_models import BaseModel
 
 
 class ShortenedUrl(BaseModel, table=True):
-    id: int = Field(primary_key=True)
-    main_url: str = Field(index=True)
+    id: int | None = Field(primary_key=True)
+    main_url: str = Field()
     short_url: str = Field(index=True)
     active: bool = Field(default=True)
+    expired: bool = Field(default=False)
     expires_at: float | None = Field()
